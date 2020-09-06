0206493e345cb2309d9f6f33cb5fe6923b284691
Juan Chehin <chehinjuan@gmail.com>
2020-09-06 18:52:17 -0300

    Primer commit

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+
+
+    $(document).ready(function() {
+     
+      $("#slider").owlCarousel({
+     
+          navigation : true, // Show next and prev buttons
+          slideSpeed : 200,
+          paginationSpeed : 400,
+          loop:true,
+          singleItem:true ,
+          transitionStyle : "fade",
+          autoPlay:true,
+          autoPlayTimeout:2000
+     
+      });
+     
+    });
+
